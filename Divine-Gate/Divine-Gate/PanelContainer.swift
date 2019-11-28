2343b90d7be6f1f98fa04c923f7ba75a2e4b56be
hnaga84 <sfbnv17@gmail.com>
2019-11-28 15:41:23 +0900

    panelcontainerファイルを作成

---
+++ Divine-Gate/Divine-Gate/PanelContainer.swift
@@ -0,0 +1,13 @@
+//
+//  PanelContainer.swift
+//  Divine-Gate
+//
+//  Created by 長濱北斗 on 2019/11/28.
+//  Copyright © 2019 宮里　佳音. All rights reserved.
+//
+
+import UIKit
+
+class PanelContainer: SKSpriteNode {
+
+}
