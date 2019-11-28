8a66a66661b287cab4b4dabe5317cc15d448e74d
KiyamaMizuki <e185758@ie.u-ryukyu.ac.jp>
2019-11-28 12:04:12 +0900

    パネルを格納するようにした

--- Divine-Gate/Divine-Gate/PanelContainer.swift
+++ Divine-Gate/Divine-Gate/PanelContainer.swift
@@ -11,18 +11,50 @@ import SpriteKit
 
 class PanelContainer: SKSpriteNode {
     
-    var panels : [Panel];
+    var panels : [Panel] = []
+    
+    var MAXLEN : Int = 5;
     
 //    Panel belong_panel;
     init(){
         super.init(texture: SKTexture(imageNamed: "Box"), color: UIColor.clear, size: CGSize(width: 100, height: 100));
-        self.position = CGPoint(x: 100, y: 100);
+        self.position = CGPoint(x: 10, y: 10);
     }
     
     func addPanel(panel : Panel){
 //        let reSize = CGSize(width: self.width * 0.5, height: self.size.height * scaleSize)
 //        self.scaleImage(scaleSize: 0.5)
         panel.size = CGSize(width: self.size.width * 0.5, height: self.size.height * 0.5);
+        panel.zPosition = self.zPosition + 1
+        if self.panels.count < MAXLEN{
+            self.panels.append(panel)
+            panel.size = CGSize(width:self.size.width/2, height:self.size.height/2)
+            switch self.panels.count{
+            case 1:
+                panel.setPosition(x: Int(self.position.x - self.size.width/4), y: Int(self.position.y + self.size.height/4))
+                //一番目のパネルを正しい場所に固定する
+            case 2:
+                print("２ ")
+                panel.setPosition(x: Int(self.position.x + self.size.width/4), y: Int(self.position.y + self.size.height/4))
+                //一番目のパネルを正しい場所に固定する
+            case 3:
+                print("３ ")
+                panel.setPosition(x: Int(self.position.x - self.size.width/4), y: Int(self.position.y - self.size.height/4))
+                //一番目のパネルを正しい場所に固定する
+            case 4:
+            print(" ")
+            panel.setPosition(x: Int(self.position.x + self.size.width/4), y: Int(self.position.y - self.size.height/4))
+            //一番目のパネルを正しい場所に固定する
+            case 5:
+                panel.setPosition(x: Int(self.position.x), y: Int(self.position.y))
+            print(" ")
+            //コンテナに持ってきたパネルを入れる
+            //一番目のパネルを正しい場所に固定する
+            default:
+            print("ifの段階で弾かれてる")
+            }
+        }
+        
     }
     
     
